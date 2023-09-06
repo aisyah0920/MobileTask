d6bf91cf1e2df818ec6ea596e88c774aae439c8e
aisyah0920 <aisyahaz.2021@student.uny.ac..id>
2023-09-06 20:38:01 +0700

    Initial commit

---
+++ app/src/main/java/com/aisyahazz/regtrack/ui/theme/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.aisyahazz.regtrack.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aisyahazz.regtrack.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
